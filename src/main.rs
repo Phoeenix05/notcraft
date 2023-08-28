e5ff135f6a1acbec56258c3d6a3a2703145ac344
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-28 17:41:55 +0300

    (feat) window and camera setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use bevy::{
+    log::LogPlugin,
+    prelude::*,
+    window::{PresentMode, WindowTheme},
+};
+
 fn main() {
     env_logger::init();
-    log::info!("Hallo, world!");
+
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.05, 0.05, 0.05)))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "NotCraft".into(),
+                        present_mode: PresentMode::Immediate,
+                        resolution: (1280.0, 800.0).into(),
+                        resizable: false,
+                        window_theme: Some(WindowTheme::Dark),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .disable::<LogPlugin>(),
+        )
+        .add_systems(Startup, setup)
+        .run();
+    log::info!("test");
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
